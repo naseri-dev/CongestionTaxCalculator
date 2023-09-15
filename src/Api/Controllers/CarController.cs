54159f4be5d145d7b774b74edbc9cbaf05cc7639
naseri-dev <dev.naseri@gmail.com>
2023-09-16 01:01:05 +0330

    Clean commented codes

--- src/Api/Controllers/CarController.cs
+++ src/Api/Controllers/CarController.cs
@@ -18,40 +18,6 @@ namespace Api.Controllers
             _mediator = mediator;
         }
 
-        //[HttpPost]
-        //public async Task<IActionResult> Create([FromBody] CreateCountryCommand createCountry)
-        //{
-        //    var id = await _mediator.Send(createCountry);
-        //    return Ok(id);
-        //}
-        //[HttpPut()]
-        //public async Task<IActionResult> Put([FromBody] EditCountryCommand editCountry)
-        //{
-        //    var id = await _mediator.Send(editCountry);
-        //    return Ok(id);
-        //}
-
-        //[HttpGet("{id:guid}")]
-        //public async Task<ActionResult<CountryDto>> Get(Guid id)
-        //{
-        //    var dto = await _mediator.Send(new GetCountryByIdQuery(id));
-        //    return Ok(dto);
-        //}
-
-        //[HttpGet()]
-        //public async Task<ActionResult<List<CountryDto>>> Get()
-        //{
-        //    var dto = await _mediator.Send(new GetCountriesQuery());
-        //    return Ok(dto);
-        //}
-
-        //[HttpDelete("{id:guid}")]
-        //public async Task<ActionResult<CountryDto>> Delete(Guid id)
-        //{
-        //    var dto = await _mediator.Send(new DeleteCountryCommand(id));
-        //    return Ok(dto);
-        //}
-
         [HttpPost("get-congestion-tax")]
         public async Task<ActionResult<CongestionTaxResult>> GetTax([FromBody] GetCongestionTaxQuery query)
         {
